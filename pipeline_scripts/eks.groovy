555cd229e589ab3748e18e2ee4a0e7a791f65517
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-13 11:39:25 +0530

    jenkisnfile

--- pipeline_scripts/eks.groovy
+++ pipeline_scripts/eks.groovy
@@ -1,1 +1,24 @@
-dvv
+pipeline {
+    agent any
+
+    stages {
+        stage('Cloning factory Repo') {
+            steps {
+                git 'https://github.com/Shyamjith06/factory.git'
+            }
+              }
+        stage('Execute Terraform to Provison EKS'){
+            steps {
+                script {
+                    sh """
+                    cd ${WORKSPACE}/factory/terraform/config/eks_setup/
+                        terraform init
+                        terraform plan
+                        
+                        terraform apply -auto-approve
+                        """
+                       }
+                    }
+            }
+        }
+}
