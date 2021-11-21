89b2315b4ff2f414812f626c3db07803359cb38e
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-11-21 13:10:59 +0530

    Create ec2_db_vpc.groovy

---
+++ pipeline_scripts/ec2_db_vpc.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Cloning factory Repo') {
+            steps {
+                git branch: 'main',
+                    credentialsId: 'Git',
+                    url: 'https://github.com/Shyamjith06/factory.git'
+                }
+              }
+        stage('Execute Terraform to Provison EKS'){
+            steps {
+                script {
+                    sh """
+                    cd ${WORKSPACE}/terraform/config/db_ec2_vpc/
+                        terraform workspace new factory
+                        terraform workspace select factory
+                        terraform init
+                        terraform workspace select ${params.env}
+                        terraform plan
+                        
+                        terraform ${params.terraform_option} -auto-approve
+                        
+                        """
+                       }
+                    }
+            }
+    }
+}
