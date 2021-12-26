a433039735a1e67c75713ea0a54ca94db70ff153
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2021-12-26 12:14:07 +0530

    Create BuildApp.groovy

---
+++ pipeline_scripts/debit_cardApp_continousIntegration/BuildApp.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent { label 'slave_one' }
+
+    stages {
+    
+        stage('Cloning the App Repo') {
+              steps { 
+                git branch: 'master',
+                    credentialsId: 'Git',
+                    url: 'https://github.com/Shyamjith06/debit_card_app.git'      
+            }          
+            }
+      stage('Build app') {
+              steps {
+              script {
+            //  withEnv(["DB_HOST=${dbHost}"]) {
+                    sh """
+                    cd ${WORKSPACE}/project_code/
+                    mvn clean install
+                  """
+             //     }
+                }
+                }
+                
+       }
+    }
+}
