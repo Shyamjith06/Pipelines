31e02a486d01cb4fdfef035c12d5ebab044faf3f
Shyamjith06 <58689812+Shyamjith06@users.noreply.github.com>
2023-06-14 15:31:55 +0530

    Update restartIIS.groovy

--- dotnetDeploy/restartIIS.groovy
+++ dotnetDeploy/restartIIS.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonSlurper
 pipeline {
     agent {
         label 'agent'
@@ -13,22 +14,41 @@ pipeline {
         // ]
     }
 
+    
+    
+    
     stages {
-        stage('Create PowerShell session') {
-            steps {
-                withCredentials([usernamePassword(credentialsId: env.CRED_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                    powershell '''
-                        $content = get-content .\\dotnetDeploy\\idev1.json -Raw
-                        $contentobject = $content | ConvertFrom-Json
-                        $remoteHost=$contentobject.bei_services
-                        write-output "$remoteHost"
-                        $session = New-PSSession -ComputerName "$remoteHost" -Credential (New-Object System.Management.Automation.PSCredential("$env:USERNAME", $(ConvertTo-SecureString "$env:PASSWORD" -AsPlainText -Force)))
-                        Invoke-Command -Session $session -ScriptBlock {start-WebAppPool -Name "DefaultAppPool"}
-                        '''
+//         stage('Create PowerShell session') {
+//             steps {
+//                 withCredentials([usernamePassword(credentialsId: env.CRED_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+//                     powershell '''
+//                         $content = get-content .\\dotnetDeploy\\idev1.json -Raw
+//                         $contentobject = $content | ConvertFrom-Json
+//                         $remoteHost=$contentobject.bei_services
+//                         write-output "$remoteHost"
+//                        # $session = New-PSSession -ComputerName $remoteHost -Credential (New-Object System.Management.Automation.PSCredential("$env:USERNAME", $(ConvertTo-SecureString "$env:PASSWORD" -AsPlainText -Force)))
+//                        # Invoke-Command -Session $session -ScriptBlock {start-WebAppPool -Name "DefaultAppPool"}
+//                         '''
+//                 }
+//             }
+//         }
+        stage ('json')
+        {
+            step{
+                script{
+                    def jsonFilePath = '.\\dotnetDeploy\\idev1.json'  // Replace with your JSON file path
+                    def jsonSlurper = new JsonSlurper()
+                    def jsonData = jsonSlurper.parse(new File(jsonFilePath))
+                    def remoteHostIPs = jsonData.hostIPs
+                    for (remoteIp in remoteHostIPs.keySet()) {
+                        env.ip = remoteIp
+                        def cred = remoteHostIPs[env.ip]
+                        echo "$remoteIp"
+// Read JSON file and parse data
+                    }
                 }
             }
         }
-        
-    
+             
     }
 }
